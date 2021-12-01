4a1c17f3826a3dcd76002ef1fee7d802103f9a47
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-01 18:29:30 +0000

    First day

---
+++ src/sonarsweep/main.go
@@ -0,0 +1,77 @@
+package sonarsweep
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func readLines(path string) ([]int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return lines, errors.New("something went wrong scanning int")
+		}
+		lines = append(lines, line)
+	}
+
+	return lines, nil
+}
+
+func Challenge1(data []int) (int, error) {
+	count := 0
+	for i := 1; i < len(data); i++ {
+		if data[i] > data[i-1] {
+			count++
+		}
+	}
+	return count, nil
+}
+
+func Challenge2(data []int) (int, error) {
+	var newData []int
+	count := 0
+	for i := 0; i < len(data)-2; i++ {
+		newData = append(newData, data[i]+data[i+1]+data[i+2])
+	}
+	for i := 1; i < len(newData); i++ {
+		if newData[i] > newData[i-1] {
+			count++
+		}
+	}
+	return count, nil
+}
+
+func Run() {
+	fmt.Println("Day 1 - Sonar Sweep")
+	path := "src/sonarsweep/input.txt"
+	data, err := readLines(path)
+	if err != nil {
+		fmt.Printf("Failed with error '%v'\n", err)
+	}
+
+	var result int
+	result, err = Challenge1(data)
+	if err != nil {
+		fmt.Printf("Error running challenge 1: %v\n", err)
+	} else {
+		fmt.Printf("Challenge 1: %d\n", result)
+	}
+
+	result, err = Challenge2(data)
+	if err != nil {
+		fmt.Printf("Error running challenge 2: %v\n", err)
+	} else {
+		fmt.Printf("Challenge 2: %d\n", result)
+	}
+}
---
+++ src/sonarsweep/main_test.go
@@ -0,0 +1,64 @@
+package sonarsweep_test
+
+import (
+	. "sonarsweep"
+	"testing"
+)
+
+func TestChallenge1(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []int
+		expected int
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263},
+			expected: 7,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge1(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge1(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge1(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge1(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestChallenge2(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []int
+		expected int
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263},
+			expected: 5,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge2(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge1(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge1(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge1(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
