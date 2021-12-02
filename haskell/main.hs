eb2d6e26d826c8268ad140670fe4bdc66257b2d0
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-02 20:53:21 +0000

    Second day challenge - Haskell version

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,4 +1,9 @@
 import SonarSweep.Challenges
+import Dive.Challenges
+import Text.Printf
 
 main = do
   SonarSweep.Challenges.run
+  printf "\n"
+  Dive.Challenges.run
+  printf "\n"
--- haskell/test.hs
+++ haskell/test.hs
@@ -1,17 +1,32 @@
 import SonarSweep.Challenges
+import Dive.Challenges
 
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  let inputData = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+  describe "Day 1" $ do
+    let inputData = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
-  describe "Challenge1" $ do
-    let result = 7
-    it "returns the right value for the sample input" $ do
-      SonarSweep.Challenges.challenge1 inputData `shouldBe` result
+    describe "Challenge1" $ do
+      let result = 7
+      it "returns the right value for the sample input" $ do
+        SonarSweep.Challenges.challenge1 inputData `shouldBe` result
 
-  describe "Challenge2" $ do
-    let result = 5
-    it "returns the right value for the sample input" $ do
-      SonarSweep.Challenges.challenge2 inputData `shouldBe` result
+    describe "Challenge2" $ do
+      let result = 5
+      it "returns the right value for the sample input" $ do
+        SonarSweep.Challenges.challenge2 inputData `shouldBe` result
+
+  describe "Day 2" $ do
+    let inputData = [Dive.Challenges.Command "forward" 5,Dive.Challenges.Command "down" 5,Dive.Challenges.Command "forward" 8,Dive.Challenges.Command "up" 3,Dive.Challenges.Command "down" 8,Dive.Challenges.Command "forward" 2]
+
+    describe "Challenge 1" $ do
+      let result = 150
+      it "returns the right value for the sample input" $ do
+        Dive.Challenges.challenge1 inputData `shouldBe` result
+
+    describe "Challenge 2" $ do
+      let result = 900
+      it "returns the right value for the sample input" $ do
+        Dive.Challenges.challenge2 inputData `shouldBe` result
