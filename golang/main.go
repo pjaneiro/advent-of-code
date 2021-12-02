f3187b0f23bf921f70b2d3dd822957b7c9025a4b
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-02 14:22:07 +0000

    Second day challenge - Golang version

--- golang/main.go
+++ golang/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/pjaneiro/advent-of-code-2021/dive"
 	"github.com/pjaneiro/advent-of-code-2021/sonarsweep"
 	"time"
 )
@@ -12,6 +13,10 @@ func main() {
 
 	timer = time.Now()
 	sonarsweep.Run()
+	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
+
+	timer = time.Now()
+	dive.Run()
 	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
 
 	fmt.Printf("Total time elapsed: %v\n", time.Since(timerAll))
