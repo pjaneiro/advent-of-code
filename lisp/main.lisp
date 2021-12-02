2c01effb3414eaa0b542c293b2b017cc40fa7ed6
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-02 23:59:24 +0000

    Second day challenge - Common Lisp version

--- lisp/main.lisp
+++ lisp/main.lisp
@@ -1,7 +1,17 @@
 (load "sonarsweep/main.lisp")
+(load "dive/main.lisp")
 (defpackage :main
   (:use :cl)
   (:import-from sonarsweep :run-sonar-sweep)
+  (:import-from dive :run-dive)
 )
 (in-package :main)
+
+(defun newline ()
+  (format t "~C" #\linefeed)
+)
+
 (run-sonar-sweep)
+(newline)
+(run-dive)
+(newline)
