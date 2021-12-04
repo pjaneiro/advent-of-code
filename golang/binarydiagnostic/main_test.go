3964056740c11b3319111a23ad5f4e6870140f69
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-04 21:27:52 +0000

    Third day challenge - Golang version

---
+++ golang/binarydiagnostic/main_test.go
@@ -0,0 +1,64 @@
+package binarydiagnostic_test
+
+import (
+	. "github.com/pjaneiro/advent-of-code-2021/binarydiagnostic"
+	"testing"
+)
+
+func TestChallenge1(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []string
+		expected int
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []string{"00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"},
+			expected: 198,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge1(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge1(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge1(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge1(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestChallenge2(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []string
+		expected int
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []string{"00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"},
+			expected: 230,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge2(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge2(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge2(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge2(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
