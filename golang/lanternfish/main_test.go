4670ae1887a6571108f53bd0ed1eaeb4168e1417
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-06 13:31:13 +0000

    Sixth day challenge - Golang version

---
+++ golang/lanternfish/main_test.go
@@ -0,0 +1,64 @@
+package lanternfish_test
+
+import (
+	. "github.com/pjaneiro/advent-of-code-2021/lanternfish"
+	"testing"
+)
+
+func TestChallenge1(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []int
+		expected int64
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []int{3, 4, 3, 1, 2},
+			expected: 5934,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge1(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge1(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge1(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge1(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestChallenge2(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []int
+		expected int64
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []int{3, 4, 3, 1, 2},
+			expected: 26984457539,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge2(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge2(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge2(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge2(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
--- golang/main.go
+++ golang/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/pjaneiro/advent-of-code-2021/dive"
 	"github.com/pjaneiro/advent-of-code-2021/giantsquid"
 	"github.com/pjaneiro/advent-of-code-2021/hydrothermalventure"
+	"github.com/pjaneiro/advent-of-code-2021/lanternfish"
 	"github.com/pjaneiro/advent-of-code-2021/sonarsweep"
 	"time"
 )
@@ -32,6 +33,10 @@ func main() {
 
 	timer = time.Now()
 	hydrothermalventure.Run()
+	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
+
+	timer = time.Now()
+	lanternfish.Run()
 	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
 
 	fmt.Printf("Total time elapsed: %v\n", time.Since(timerAll))
