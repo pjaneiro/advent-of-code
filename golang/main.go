bbb0b2af64fe0cdeb80b8a844e8b9758583315c1
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-07 17:48:08 +0000

    Seventh day challenge - Golang version

--- golang/main.go
+++ golang/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/pjaneiro/advent-of-code-2021/hydrothermalventure"
 	"github.com/pjaneiro/advent-of-code-2021/lanternfish"
 	"github.com/pjaneiro/advent-of-code-2021/sonarsweep"
+	"github.com/pjaneiro/advent-of-code-2021/thetreacheryofwhales"
 	"time"
 )
 
@@ -37,6 +38,10 @@ func main() {
 
 	timer = time.Now()
 	lanternfish.Run()
+	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
+
+	timer = time.Now()
+	thetreacheryofwhales.Run()
 	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
 
 	fmt.Printf("Total time elapsed: %v\n", time.Since(timerAll))
---
+++ golang/thetreacheryofwhales/main_test.go
@@ -0,0 +1,64 @@
+package thetreacheryofwhales_test
+
+import (
+	. "github.com/pjaneiro/advent-of-code-2021/thetreacheryofwhales"
+	"testing"
+)
+
+func TestChallenge1(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []int
+		expected int
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []int{16, 1, 2, 0, 4, 2, 7, 1, 2, 14},
+			expected: 37,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge1(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge1(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge1(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge1(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestChallenge2(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []int
+		expected int
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []int{16, 1, 2, 0, 4, 2, 7, 1, 2, 14},
+			expected: 168,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge2(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge2(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge2(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge2(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
