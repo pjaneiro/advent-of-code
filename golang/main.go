7171521f13f0f7b4839fc9eb72a552c6e4da1f30
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-11 22:47:19 +0000

    Eleventh day challenge - Golang version

--- golang/main.go
+++ golang/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/pjaneiro/advent-of-code-2021/binarydiagnostic"
 	"github.com/pjaneiro/advent-of-code-2021/dive"
+	"github.com/pjaneiro/advent-of-code-2021/dumbooctopus"
 	"github.com/pjaneiro/advent-of-code-2021/giantsquid"
 	"github.com/pjaneiro/advent-of-code-2021/hydrothermalventure"
 	"github.com/pjaneiro/advent-of-code-2021/lanternfish"
@@ -57,6 +58,10 @@ func main() {
 
 	timer = time.Now()
 	syntaxscoring.Run()
+	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
+
+	timer = time.Now()
+	dumbooctopus.Run()
 	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
 
 	fmt.Printf("Total time elapsed: %v\n", time.Since(timerAll))
