f8e6cebd9f706fdd715f437590f556e973b947f7
Pedro Janeiro <pjaneiro@whitesmith.co>
2025-12-01 15:32:45 +0000

    Save WIP from 2024

--- 2024/main.go
+++ 2024/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pjaneiro/advent-of-code/2024/ceressearch"
 	"github.com/pjaneiro/advent-of-code/2024/historianhysteria"
 	"github.com/pjaneiro/advent-of-code/2024/mullitover"
+	"github.com/pjaneiro/advent-of-code/2024/printqueue"
 	"github.com/pjaneiro/advent-of-code/2024/rednosedreports"
 )
 
@@ -28,6 +29,10 @@ func main() {
 
 	timer = time.Now()
 	ceressearch.Run()
+	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
+
+	timer = time.Now()
+	printqueue.Run()
 	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
 
 	fmt.Printf("Total time elapsed: %v\n", time.Since(timerAll))
---
+++ 2024/printqueue/main.go
@@ -0,0 +1,56 @@
+package printqueue
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+)
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, nil
+}
+
+func Challenge1(data []string) (int, error) {
+	return 0, errors.New("not implemented yet")
+}
+
+func Challenge2(data []string) (int, error) {
+	return 0, errors.New("not implemented yet")
+}
+
+func Run() {
+	fmt.Println("Day 5 - Print Queue")
+	path := "printqueue/input.txt"
+	data, err := readLines(path)
+	if err != nil {
+		fmt.Printf("Failed with error '%v'\n", err)
+	}
+
+	var result int
+	result, err = Challenge1(data)
+	if err != nil {
+		fmt.Printf("Error running challenge 1: %v\n", err)
+	} else {
+		fmt.Printf("Challenge 1: %d\n", result)
+	}
+
+	result, err = Challenge2(data)
+	if err != nil {
+		fmt.Printf("Error running challenge 2: %v\n", err)
+	} else {
+		fmt.Printf("Challenge 2: %d\n", result)
+	}
+}
