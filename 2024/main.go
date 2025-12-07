eb54e2e5f2e87c71f82bea9791ed4d3264f1e5a7
Pedro Janeiro <pjaneiro@whitesmith.co>
2025-12-07 23:50:31 +0000

    2024 - Sixth day

--- 2024/main.go
+++ 2024/main.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/pjaneiro/advent-of-code/2024/ceressearch"
+	"github.com/pjaneiro/advent-of-code/2024/guardgallivant"
 	"github.com/pjaneiro/advent-of-code/2024/historianhysteria"
 	"github.com/pjaneiro/advent-of-code/2024/mullitover"
 	"github.com/pjaneiro/advent-of-code/2024/printqueue"
@@ -33,6 +34,10 @@ func main() {
 
 	timer = time.Now()
 	printqueue.Run()
+	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
+
+	timer = time.Now()
+	guardgallivant.Run()
 	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
 
 	fmt.Printf("Total time elapsed: %v\n", time.Since(timerAll))
