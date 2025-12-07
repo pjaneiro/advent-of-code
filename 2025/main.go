f7bf2886422e212b40a71ec620e9842ae018d687
Pedro Janeiro <pjaneiro@whitesmith.co>
2025-12-07 18:04:24 +0000

    2025 - Seventh day

--- 2025/main.go
+++ 2025/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/pjaneiro/advent-of-code/2025/cafeteria"
 	"github.com/pjaneiro/advent-of-code/2025/giftshop"
+	"github.com/pjaneiro/advent-of-code/2025/laboratories"
 	"github.com/pjaneiro/advent-of-code/2025/lobby"
 	"github.com/pjaneiro/advent-of-code/2025/printingdepartment"
 	"github.com/pjaneiro/advent-of-code/2025/secretentrance"
@@ -38,6 +39,10 @@ func main() {
 
 	timer = time.Now()
 	trashcompactor.Run()
+	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
+
+	timer = time.Now()
+	laboratories.Run()
 	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
 
 	fmt.Printf("Total time elapsed: %v\n", time.Since(timerAll))
