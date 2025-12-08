eccd7a0630da43ee94f14febca7df8383267b2c7
Pedro Janeiro <pjaneiro@whitesmith.co>
2025-12-08 02:22:14 +0000

    2024 - Ninth day

--- 2024/main.go
+++ 2024/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/pjaneiro/advent-of-code/2024/bridgerepair"
 	"github.com/pjaneiro/advent-of-code/2024/ceressearch"
+	"github.com/pjaneiro/advent-of-code/2024/diskfragmenter"
 	"github.com/pjaneiro/advent-of-code/2024/guardgallivant"
 	"github.com/pjaneiro/advent-of-code/2024/historianhysteria"
 	"github.com/pjaneiro/advent-of-code/2024/mullitover"
@@ -48,6 +49,10 @@ func main() {
 
 	timer = time.Now()
 	resonantcollinearity.Run()
+	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
+
+	timer = time.Now()
+	diskfragmenter.Run()
 	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
 
 	fmt.Printf("Total time elapsed: %v\n", time.Since(timerAll))
