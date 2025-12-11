49436b992b6fbb682453e82c453992bccc53efab
Pedro Janeiro <pjaneiro@whitesmith.co>
2025-12-11 03:27:54 +0000

    2025 - Tenth day

---
+++ 2025/factory/main_test.go
@@ -0,0 +1,65 @@
+package factory_test
+
+import (
+	"testing"
+
+	. "github.com/pjaneiro/advent-of-code/2025/factory"
+)
+
+func TestChallenge1(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []string
+		expected int
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []string{"[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}", "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}", "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"},
+			expected: 7,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge1(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge1(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge1(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge1(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestChallenge2(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []string
+		expected int
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []string{"[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}", "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}", "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"},
+			expected: 33,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge2(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge2(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge2(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge2(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
--- 2025/go.mod
+++ 2025/go.mod
@@ -1,3 +1,5 @@
 module github.com/pjaneiro/advent-of-code/2025
 
 go 1.23
+
+require github.com/aclements/go-z3 v0.0.0-20220809013456-4675d5f90ca5
